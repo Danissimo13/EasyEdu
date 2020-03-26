a8020b810b3a9d15663fa7b75e6b942fd9fdcef8
Danissimo13 <danissimokuvshin13@gmail.com>
2020-03-27 02:02:57 +0300

    Made Get method for PersonalChat page

--- VirtualSchool/Pages/PersonalChat.cshtml.cs
+++ VirtualSchool/Pages/PersonalChat.cshtml.cs
@@ -4,17 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using VirtualSchool.Models;
+using VirtualSchool.Services;
 
 namespace VirtualSchool.Pages
 {
-    public class PmModel : PageModel
+    public class PersonalChatModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public int? Id { get; set; }
+
         public User Companion { get; set; } //С кем идёт переписка
+        public List<PMessage> Messages { get; set; }
 
-        public void OnGet()
+        private VSContext db;
+        private UserService userService;
+
+        public PersonalChatModel(VSContext context, UserService uService)
         {
+            db = context;
+            userService = uService;
+        }
 
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (!Id.HasValue)
+            {
+                return RedirectToPage("NotFound");
+            }
+
+            Companion = await userService.GetUserAsync(Id.Value);
+
+            if(Companion == null)
+            {
+                return RedirectToPage("NotFound");
+            }
+
+            Messages = await db.PMessages.Where(m => ((m.RecipientId.ToString() == User.Identity.Name) && (m.AuthorId == Companion.UserId)) ||
+                                                     ((m.RecipientId == Companion.UserId) && (m.AuthorId.ToString() == User.Identity.Name))).ToListAsync();
+            Messages.Sort((f, s) => f.PMessageId > s.PMessageId ? 1 : -1);
+
+            return Page();
         }
     }
 }
