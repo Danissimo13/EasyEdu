c5f982b24cc951718b255c2d77a72d78732823f3
Danissimo13 <danissimokuvshin13@gmail.com>
2020-03-26 19:53:59 +0300

    Rename Pm to PersonalChat

--- VirtualSchool/Pages/PersonalChat.cshtml.cs
+++ VirtualSchool/Pages/PersonalChat.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VirtualSchool.Models;
 
 namespace VirtualSchool.Pages
 {
     public class PmModel : PageModel
     {
+        public User Companion { get; set; } //С кем идёт переписка
+
         public void OnGet()
         {
 
