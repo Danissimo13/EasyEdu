ebe1f580026af3d434d8621021f0f5b164b4c54c
Danissimo13 <danissimokuvshin13@gmail.com>
2020-03-27 01:59:37 +0300

    Changed PM model

---
+++ VirtualSchool/Models/PMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualSchool.Models
+{
+    public class PMessage
+    {
+        public int PMessageId { get; set; }
+
+        public string Text { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public int RecipientId { get; set; }
+    }
+}
