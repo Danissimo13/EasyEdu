4e9cf1e7710160cb0fd8b171d24c45b82ca48e08
Danissimo13 <danissimokuvshin13@gmail.com>
2020-03-27 02:17:52 +0300

    Made Enter and Send methods for PersonalChatHub

--- VirtualSchool/Hubs/PersonalChatHub.cs
+++ VirtualSchool/Hubs/PersonalChatHub.cs
@@ -3,14 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VirtualSchool.Models;
 
 namespace VirtualSchool.Hubs
 {
     public class PersonalChatHub : Hub
     {
-        public PersonalChatHub()
-        {
+        private VSContext db;
 
+        public PersonalChatHub(VSContext context)
+        {
+            db = context;
+        }
+
+        public async Task Enter()
+        {
+            await Clients.Caller.SendAsync(Context.UserIdentifier);
+        }
+
+        public async Task Send(string toId, string message)
+        {
+            await Clients.Caller.SendAsync("Message", message);
+            await Clients.User(toId).SendAsync("Message", message);
+            await db.PMessages.AddAsync(new PMessage()
+            {
+                AuthorId = int.Parse(Context.UserIdentifier),
+                RecipientId = int.Parse(toId)
+            });
+            await db.SaveChangesAsync();
         }
     }
 }
